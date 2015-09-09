d27bb32dae1df87ebfc6e7896cc457a10d58e677
Denisse Cuarteros <dwc3@email.arizona.edu>
2015-09-09 15:33:36 -0700

    homework2

--- homework2/singlequotes.pl
+++ homework2/singlequotes.pl
@@ -3,14 +3,14 @@ use strict;
 
 my $classname  ="Genesis 1:26-27";
 print 'Hello there. Have you heard about God the Mother?\n';
-my $you =Charlie;
+my $you ='Charlie';
 chomp($you);
 print 'Hello $you, please read $classname.\n';
 print '$you\t';
 print 'from the very beginning $classname\n';
-print 'allows us to know God's will\n';
+print 'allows us to know God\'s will\n';
 print 'through what has been made.\n';
-print '$you, we're created in God's image.\n';
+print '$you, we\'re created in God\'s image.\n';
 print '$you, then according to\t';
 print '$classname, how many images were created?\n';
 print 'God has \t'; 
--- homework2/variable.pl
+++ homework2/variable.pl
@@ -1,6 +1,8 @@
 #!/user/bin/env perl
-$classname = "ABE 487";
+use strict;
+use warnings;
+
+my $classname = "ABE 487";
 print "Hello there. Welcome to Tucson.\n";
-$you = Denisse;
-chomp($you);
+my $you ='Denisse';
 print "Hello, $you. Welcome to $classname.\n";
