982c5abf668d8743ff0727f938e74d491225871e
Denisse Cuarteros <dwc3@email.arizona.edu>
2015-09-30 15:48:29 -0700

    this is my real homework4 for perl

---
+++ perlIII/p5.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature 'say';
+use autodie;
+
+while (my $line = <>) {
+chomp ($line);
+
+$line = uc $line;
+  my $nobody_index = index($line,'NOBODY');
+my $somebody_index = index($line,'SOMEBODY');
+if ($nobody_index >= 0) {
+warn ("Nobody is here: $nobody_index.\n");
+}
+if ($somebody_index >= 0) {
+warn ("Somebody is here: $somebody_index.\n");
+}
+}
