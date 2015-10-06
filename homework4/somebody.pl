d72374c5bdb20730c637108e357874d1dd1120f0
Ken Youens-Clark <kyclark@gmail.com>
2015-10-05 17:02:35 -0700

    suggestions

--- homework4/somebody.pl
+++ homework4/somebody.pl
@@ -1,18 +1,30 @@
 #!/usr/bin/env perl
+
 use strict;
 use warnings;
 
-while (my $line = <>) {
-chomp($line);
+@ARGV or die 'No input';
 
-$line = uc $line;
-my $nobody_index = index($line, 'NOBODY');
-my $somebody_index = index($line, 'SOMEBODY');
-if ($nobody_index >= 0) 
-{
-warn ("Nobody is here: $nobody_index.\n");
+while (my $line = <>) {
+    chomp($line);
+
+    $line = uc $line;
+    my $nobody_index   = index($line, 'NOBODY');
+    my $somebody_index = index($line, 'SOMEBODY');
+
+    if ($nobody_index >= 0) {
+        warn("Nobody is here: $nobody_index.\n");
+    }
+
+    if ($somebody_index >= 0) {
+        warn("Somebody is here $somebody_index.\n");
+    }
 }
-if ($somebody_index >= 0) {
-warn ("Somebody is here $somebody_index.\n");
-}
-}
+
+__END__
+
+Format your code!
+
+You never check that there is an argument, so you read from 
+STDIN and that just hangs with no direction to the user as to
+what to do.
--- homework4/uppercase.pl
+++ homework4/uppercase.pl
@@ -1,18 +1,26 @@
 #!/usr/bin/env perl
+
 use strict;
 use warnings;
+use autodie;
 
-my $in_file = shift;
+my $in_file  = shift;
 my $out_file = shift;
-my $infile = "Perl_III.nobody.txt";
-my $outfile = "uppercase.out";
+my $infile   = "Perl_III.nobody.txt";
+my $outfile  = "uppercase.out";
 
-open IN, "<", $infile or die "Error writing outfile: $!\n";
-open OUT, ">", $outfile or die "Error writing outfile: $!\n";
+open my $IN,  "<", $infile  or die "Error writing outfile: $!\n";
+open my $OUT, ">", $outfile or die "Error writing outfile: $!\n";
 
-while (my $line = <IN>) 
-{
-chomp $line;
-my $upper_case_line = uc $line;
-print OUT "$upper_case_line\n";
+while (my $line = <$IN>) {
+    my $upper_case_line = uc $line;
+    print $OUT "$upper_case_line";
 }
+
+__END__
+
+Format your code!
+
+autodie/globals
+
+Don't chomp if you're going to print the newline.
