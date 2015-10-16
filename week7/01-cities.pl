2d4cf85fa8783a87b0e98c23cb6f1e0758463428
Denisse Cuarteros <dwc3@email.arizona.edu>
2015-10-16 15:04:45 -0700

    week 7 hmwk

---
+++ week7/01-cities.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use autodie;
+use feature 'say';
+use Data::Dumper;
+
+my %homes = (
+    'Tucson'      => 'AZ',
+    'Riverside'   => 'CA',
+    'Lafayette'   => 'IN',
+);
+
+say Dumper(\%homes);
+
+foreach my $city (sort keys %homes) {
+    my $aa = $homes{$city};
+    print "$city, $aa \n";
+}
---
+++ week7/02-fasta-hash.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature 'say';
+use Data::Dumper;
+use autodie;
+
+my $file = shift @ARGV || "Perl_V.genesAndSeq.txt";
+open my $fh, '<', $file;
+my %hash;
+
+while (my $line = <$fh>){
+    chomp $line;
+    my ($key, $value) = split/\t/, $line;
+    $hash{$key} = $value;
+}
+
+for my $key (sort keys %hash){
+    my $value = $hash{$key};
+    print "key: $key value:$value \n";
+}
+
