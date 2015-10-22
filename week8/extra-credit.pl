2cab3bab738f229e138988854198ec6666ada42e
Denisse Cuarteros <dwc3@email.arizona.edu>
2015-10-22 00:27:25 -0700

    homework8 done

--- week8/extra-credit.pl
+++ week8/extra-credit.pl
@@ -5,10 +5,16 @@ use autodie;
 use feature 'say';
 use Data::Dumper;
 
-my $file = shift || die "Please provide a sequence.\n";
-my $seq = "";
-my $sc = 0;
+my $file = shift || "rebase.txt";
+open my $fh, '<', $file;
 
-open(my $IN, '<', $file);
-
-
+my %enzymes;
+while (my $line = <$fh>) {
+  if ($line =~ /^([\w\s\(\)]+)\s+\b([A-Z\^]+)$/) {
+     my ($name, $val) = ($1, $2);
+        $name =~ s/\s*$//;
+        $enzymes{ $name } = $val;
+    }
+}
+say STDERR Dumper(\%enzymes);
+printf "There are %s enzymes.\n", scalar keys %enzymes;
