29dc864401faa394e17e2c7aa0677bb7cef70c16
Denisse Cuarteros <dwc3@email.arizona.edu>
2015-11-12 00:53:25 -0700

    edited

--- week11/01-bio-searchio.pl
+++ week11/01-bio-searchio.pl
@@ -12,12 +12,12 @@ my $searchIO_obj = Bio::SearchIO->new(
                                 );
 
 while( $result_obj = $searchIO_obj->next_result ) {
-    while( my $hit = $result->next_hit ) {
-        while( my $hsp = $hit->next_hsp) {
-            if( $hsp->num_hits >= 1e-50) {
-                if( $hsp->query_string)  {
-                    if( $hsp->evalue)    {
-                        if( $hsp->hit_string) { 
+    while( my $hit = $result->next_hit )  {
+        while( my $hsp = $hit->next_hsp ) {
+            if( $hsp->num_hits >= 1e-50 ) {
+                if( $hsp->query_string )  {
+                    if( $hsp->evalue )    {
+                        if( $hsp->hit_string ) { 
                         print "Query Name=",    $result->query_name,
                              " Hit Name=",      $hit->hit_string,
                              " HSP Evalue=",    $hsp->evalue, "\n";
